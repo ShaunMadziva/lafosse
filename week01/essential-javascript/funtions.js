bc1c990a55cb5fd8b9c0017cdf2abf585fe0c318
Shaun <shaunmadziva@gmail.com>
2025-03-25 16:08:08 +0000

    functions

---
+++ week01/essential-javascript/funtions.js
@@ -0,0 +1,14 @@
+function getLarger(a,b) {
+    if (a>b) {
+        console.log("a is larger")
+    }
+}
+
+
+const getSmaller = (a,b) => {
+    if (a>b) {
+        console.log("b is smaller")
+    }
+}
+
+const getLarger2 = (a, b) => a > b ? a : b // if a is greater than b return a else return b
